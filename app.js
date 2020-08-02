fa6cba188ace934c59a31ca69f88964bb258a2fd
abhiraj-chatterjee <abhirajchatterjee@gmail.com>
2020-08-02 16:52:02 +0530

    setup routes

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const tweets = require('./routes/api/tweets');
+const bodyParser = require('body-parser');
 
 const app = express();
 const db = require('./config/keys').mongoURI;
 
+// Middleware for body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 mongoose
-    .connect(db, { useNewUrlParser: true })
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
 app.get("/", (req,res) => res.send("Hello World!"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
